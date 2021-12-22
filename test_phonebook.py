e81a9c2b58e76b57bd38e501340e1e3b7bc74c4c
Sergio Sum <Sergio@sergios-macbook.lan>
2021-12-22 11:08:54 -0500

    finished first chapter of course

---
+++ test_phonebook.py
@@ -0,0 +1,40 @@
+import unittest
+
+from phonebook import PhoneBook 
+
+class PhoneBookTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.phonebook = PhoneBook()
+
+    def tearDown(self) -> None:
+        pass
+    # Test lookup()
+    def test_lookup_by_name(self):
+        self.phonebook.add("Bob", "12345")
+        number = self.phonebook.lookup("Bob")
+        self.assertEqual("12345", number)
+
+    # Test name not in phonebook
+
+    def test_missing_name(self):
+        with self.assertRaises(KeyError):
+           self.phonebook.lookup("missing") 
+
+    # test for consistency
+    def test_empty_phonebook_is_consistent(self):
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_is_consistent_with_different_entries(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Anna", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_inconsistent_with_duplicate_entires(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Sue", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_inconsistent_with_deplicate_prefix(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Sue", "123")
+        self.assertFalse(self.phonebook.is_consistent())
